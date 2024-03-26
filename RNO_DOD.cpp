9f4eb40a3d265da4c47c4daa037c3e6bf51266cd
mjazlowiecki <146663134+mjazlowiecki@users.noreply.github.com>
2024-03-27 00:27:05 +0100

    549-RNO_DOD

---
+++ RNO_DOD.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	int liczba_testow, liczba_zmiennych, suma;
+	int zmienna; 
+
+	std::cin >> liczba_testow;
+
+	for (int i = 0; i < liczba_testow; i++)
+	{
+		std::cin >> liczba_zmiennych;
+		suma = 0;
+		for (int k = 0; k < liczba_zmiennych; k++)
+		{
+			std::cin>> zmienna;
+			suma += zmienna; 
+		}
+		std::cout << suma << std::endl; 
+		
+	}
+	return 0; 
+}
+
