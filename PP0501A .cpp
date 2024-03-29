0d77f15bc4f2c889a6ef112b645acda2ce60f35e
mjazlowiecki <146663134+mjazlowiecki@users.noreply.github.com>
2024-03-30 00:18:55 +0100

    601-PP0501A

---
+++ PP0501A .cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int nwd(int liczba_A, int liczba_B)
+{
+    while (liczba_B)
+    {
+        if (liczba_A == liczba_B)
+        {
+            return liczba_A;
+        }
+        else if (liczba_A > liczba_B)
+        {
+            liczba_A = liczba_A - liczba_B;
+        }
+        else if (liczba_B > liczba_A)
+        {
+            liczba_B = liczba_B - liczba_A;
+        }
+    }
+};
+
+int main()
+{
+    int liczba_testow;
+    int a, b;
+
+    cin >> liczba_testow;
+    for (int i = 0; i < liczba_testow; i++)
+    {
+        cin >> a >> b;
+        cout << nwd(a, b) << endl;
+    }
+    return 0;
+}
+
