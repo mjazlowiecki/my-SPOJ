724d8ec4ee6b059dcab8072f453f55dd7c27735f
mjazlowiecki <146663134+mjazlowiecki@users.noreply.github.com>
2024-11-21 00:38:47 +0100

    PP0502B

---
+++ PP0502B - Tablice.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int l_testow;
+    int l_liczb;
+    const int Max = 100;
+    int tablica[Max]; //n(max) = 100
+
+    cin >> l_testow;
+
+    for (int i = 0; i < l_testow; i++)
+    {
+        cin >> l_liczb;
+        for (int k = 0; k < l_liczb; k++)
+        {
+            cin >> tablica[k];
+        }
+        for (int j = (l_liczb -1); j >=0; j--)
+        {
+            cout << tablica[j] << " ";
+        }
+        cout << endl;
+    }
+        return 0;
+    }
